9790ba5a4572b51fd367524093fa8536b195d42a
MarineP <marine.porret@gmail.com>
2025-05-30 17:24:09 +0200

    ✨ Add API to subscribe topics

--- back/src/main/java/com/openclassrooms/mddapi/mapper/TopicMapper.java
+++ back/src/main/java/com/openclassrooms/mddapi/mapper/TopicMapper.java
@@ -1,18 +1,29 @@
 package com.openclassrooms.mddapi.mapper;
 
 import com.openclassrooms.mddapi.dto.TopicDTO;
+import com.openclassrooms.mddapi.dto.TopicsDTO;
 import com.openclassrooms.mddapi.model.Topic;
 import org.springframework.stereotype.Component;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Component
 @Mapper(componentModel = "spring")
 public interface TopicMapper {
-    TopicDTO toDto(Topic topic);
+    @Mapping(target = "isSubscribe", ignore = true)
+    TopicsDTO toDto(Topic topic);
+    @Mapping(target = "isSubscribe", ignore = true)
+    List<TopicsDTO> toDtoList(List<Topic> listTopic);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     Topic toEntity(TopicDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Topic toEntity(TopicsDTO dto);
 }
--- back/src/main/java/com/openclassrooms/mddapi/service/TopicService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/TopicService.java
@@ -1,24 +1,58 @@
 package com.openclassrooms.mddapi.service;
 
 import com.openclassrooms.mddapi.dto.TopicDTO;
+import com.openclassrooms.mddapi.dto.TopicsDTO;
 import com.openclassrooms.mddapi.mapper.TopicMapper;
+import com.openclassrooms.mddapi.model.Subscription;
 import com.openclassrooms.mddapi.model.Topic;
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.repository.SubscriptionRepository;
 import com.openclassrooms.mddapi.repository.TopicRepository;
+import com.openclassrooms.mddapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class TopicService {
     private final TopicRepository topicRepository;
+    private final SubscriptionRepository subscriptionRepository;
+    private final UserRepository userRepository;
     private final TopicMapper mapper;
 
-    public List<Topic> findAllTopics(){
-        return topicRepository.findAll();
+    public List<TopicsDTO> findAllTopics(Authentication authentication){
+        User user = userRepository.findByEmail(authentication.getName())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        List<TopicsDTO> topics = mapper.toDtoList(topicRepository.findAll());
+
+        for (TopicsDTO topicDTO: topics) {
+            Topic topic = topicRepository.findById(topicDTO.getId())
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Topic not found"));
+            Optional<Subscription> subscription = subscriptionRepository.findByUserAndTopic(user, topic);
+            topicDTO.setIsSubscribe(subscription.isPresent());
+        }
+
+        return topics;
     }
+
     public void createTopic (TopicDTO topicDTO) {
         topicRepository.save(mapper.toEntity(topicDTO));
+    }
+
+    public List<TopicsDTO> subscribeTopic(Authentication authentication, Integer topicId){
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+        Topic topic = topicRepository.findById(topicId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Topic not found"));
+        Subscription subscription = new Subscription(user, topic);
+        subscriptionRepository.save(subscription);
+
+        return findAllTopics(authentication);
     }
 }
